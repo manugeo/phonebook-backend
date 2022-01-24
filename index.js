fe114fc058f0399822a360e5638bfd4dd7d0492d
Manu <manugeo13@gmail.com>
2022-01-24 13:34:57 +0530

    Exercise 3.4 - Phonebook step 4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const persons = [
+let persons = [
   {
     "id": 1,
     "name": "Arto Hellas",
@@ -45,6 +45,13 @@ app.get('/api/persons/:id', (request, response) => {
     response.statusMessage = `Can't find a person with id ${id}.`;
     response.status(404).end();
   }
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(p => (p.id !== id));
+  console.log(`Deleted person with id: ${id}.`);
+  response.status(204).end();
 });
 
 const PORT = 3001
